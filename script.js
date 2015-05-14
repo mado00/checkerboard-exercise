7a489efbf9e843b09cc9cba82a3fc4330c3a3600
madoka <menomonies@gmail.com>
2015-05-14 01:00:05 -0700

    Submit

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Your JS goes here
+
+function checkerBoard() {
+	// creating div
+	var mainDiv = document.createElement("div");
+	document.body.appendChild(mainDiv);
+	for(var i = 0; i < 9; i++) {
+		for(var j = 0; j < 9; j++) {
+			var div = document.createElement("div");
+			div.style.width = "11.1%";
+			div.style.paddingBottom = "11.1%";
+			div.style.float = "left";
+			// when even number: red, odd number : black
+			div.style.backgroundColor = (j+i) % 2 ? "red" : "black";
+			mainDiv.appendChild(div);
+		}
+		var clearDiv = document.createElement("div");
+		clearDiv.style.clear = "both";
+		mainDiv.appendChild(clearDiv);
+	}
+}
+checkerBoard();
