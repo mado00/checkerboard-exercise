182d3ca13cea6e3df75c490be7834f50c7319d52
madoka <menomonies@gmail.com>
2015-05-14 19:51:16 -0700

    Fix flashing

--- script.js
+++ script.js
@@ -4,35 +4,36 @@
 
 function checkerBoard() {
 	// get mainDiv with id 'main'
-	var main = document.getElementById("div");
-	if(main) {
-		div.innerHTML = "";
+	var mainDiv = document.getElementById("main");
+	if(mainDiv) {
+		mainDiv.innerHTML = "";
 	} else {
 	// creating div
 		var mainDiv = document.createElement("div");
+		mainDiv.id = "main";
 		document.body.appendChild(mainDiv);
-		for(var i = 0; i < 9; i++) {
-			for(var j = 0; j < 9; j++) {
-				var div = document.createElement("div");
-				div.style.width = "11.1%";
-				div.style.paddingBottom = "11.1%";
-				div.style.float = "left";
-				// when even number: red, odd number : black
-				var color = "rgb(255," + (j%2 ? 255 : 0) + ","+ (i*28) + ")";
-				console.log(color);
-				div.style.backgroundColor = color;
-				mainDiv.appendChild(div);
-			}
-			var clearDiv = document.createElement("div");
-			clearDiv.style.clear = "both";
-	 		mainDiv.appendChild(clearDiv);
+	}
+	for(var i = 0; i < 9; i++) {
+		for(var j = 0; j < 9; j++) {
+			var div = document.createElement("div");
+			div.style.width = "11.1%";
+			div.style.paddingBottom = "11.1%";
+			div.style.float = "left";
+			// when even number: red, odd number : black
+			var color = "rgb(" + Math.floor(Math.random() * 255) + "," + (j%2 ? 255 : 0) + ","+ (i*28) + ")";
+			console.log(color);
+			div.style.backgroundColor = color;
+			mainDiv.appendChild(div);
 		}
+		var clearDiv = document.createElement("div");
+		clearDiv.style.clear = "both";
+ 		mainDiv.appendChild(clearDiv);
 	}
 }
 
-checkerBoard();
+//checkerBoard();
 
-
+//flashing is not working
 function flashing() {
 	randInterval = window.setInterval(checkerBoard, 2000);
 }
