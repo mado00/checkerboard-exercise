2e12acb3be1136df58cef6523cd41ee938b49766
madoka <menomonies@gmail.com>
2015-05-14 18:44:01 -0700

    Submit flash

--- script.js
+++ script.js
@@ -3,26 +3,38 @@
 //gradient
 
 function checkerBoard() {
+	// get mainDiv with id 'main'
+	var main = document.getElementById("div");
+	if(main) {
+		div.innerHTML = "";
+	} else {
 	// creating div
-	var mainDiv = document.createElement("div");
-	document.body.appendChild(mainDiv);
-	for(var i = 0; i < 9; i++) {
-		for(var j = 0; j < 9; j++) {
-			var div = document.createElement("div");
-			div.style.width = "11.1%";
-			div.style.paddingBottom = "11.1%";
-			div.style.float = "left";
-			// when even number: red, odd number : black
-			var color = "rgb(255," + (j%2 ? 255 : 0) + ","+ (i*28) + ")";
-			console.log(color);
-			div.style.backgroundColor = color;
-			mainDiv.appendChild(div);
+		var mainDiv = document.createElement("div");
+		document.body.appendChild(mainDiv);
+		for(var i = 0; i < 9; i++) {
+			for(var j = 0; j < 9; j++) {
+				var div = document.createElement("div");
+				div.style.width = "11.1%";
+				div.style.paddingBottom = "11.1%";
+				div.style.float = "left";
+				// when even number: red, odd number : black
+				var color = "rgb(255," + (j%2 ? 255 : 0) + ","+ (i*28) + ")";
+				console.log(color);
+				div.style.backgroundColor = color;
+				mainDiv.appendChild(div);
+			}
+			var clearDiv = document.createElement("div");
+			clearDiv.style.clear = "both";
+	 		mainDiv.appendChild(clearDiv);
 		}
-		var clearDiv = document.createElement("div");
-		clearDiv.style.clear = "both";
- 		mainDiv.appendChild(clearDiv);
-
 	}
 }
 
 checkerBoard();
+
+
+function flashing() {
+	randInterval = window.setInterval(checkerBoard, 2000);
+}
+
+flashing();
